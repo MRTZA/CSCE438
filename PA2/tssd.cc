a9f534c39c6d4496f29721a4ffde844f290dac94
Murtaza Hakimi <murtaza.hakimi@gmail.com>
2019-02-11 18:16:54 -0600

    starting proto file

--- PA2/tssd.cc
+++ PA2/tssd.cc
@@ -10,28 +10,28 @@ using grpc::ServerContext;
 using grpc::Status;
 
 void RunServer() {
-//   std::string server_address("0.0.0.0:50051");
+  std::string server_address("0.0.0.0:50051");
 
-//   ServerBuilder builder;
+  ServerBuilder builder;
 
-//   // Listen on the given address without any authentication mechanism.
-//   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
 
-//   // Register "service" as the instance through which we'll communicate with
-//   // clients. In this case it corresponds to an *synchronous* service.
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
 //   builder.RegisterService(&service);
 
-//   // Finally assemble the server.
-//   std::unique_ptr<Server> server(builder.BuildAndStart());
-//   std::cout << "Server listening on " << server_address << std::endl;
+  // Finally assemble the server.
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
 
-//   // Wait for the server to shutdown. Note that some other thread must be
-//   // responsible for shutting down the server for this call to ever return.
-//   server->Wait();
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
 }
 
 int main(int argc, char** argv) {
-//   RunServer();
+  RunServer();
 
   return 0;
 }

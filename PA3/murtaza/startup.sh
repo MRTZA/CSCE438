3795869c69fdc0e45380e4b9cb1f88e7481ed0b2
Bradley Kost <bradleykost@gmail.com>
2019-04-02 18:33:09 -0500

    Start up script changes

--- PA3/murtaza/startup.sh
+++ PA3/murtaza/startup.sh
@@ -3,21 +3,24 @@ clear
 
 masterPort = 3010
 slavePort = 3011
+availableIP = 10.0.2.5
+masterOneIP = 10.0.2.6
+masterTwoIP = 10.0.2.7
 
-available = 10.0.2.5:$masterPort
-masterOne = 10.0.2.6:$masterPort
-masterTwo = 10.0.2.7:$masterPort
+available = $availableIP:$masterPort
+masterOne = $masterOneIP:$masterPort
+masterTwo = $masterTwoIP:$masterPort
 
 echo "Are you starting the routing server? (y/n): "
 read isrouter
 
 if [ $isrouter = "y" ]
 then
-    ./tsd -r router -a $available -m $masterOne -n $masterTwo -o $slavePort
-    ./tsd -p $slavePort -r slave -o $masterPort
+    ./tsd -r router -a $available -m $masterOne -n $masterTwo -o $slavePort &
+    ./tsd -p $slavePort -r slave -o $masterPort &
 else 
-    ./tsd -p $masterPort -r master -o $slavePort
-    ./tsd -p $slavePort -r slave -o $masterPort
+    ./tsd -p $masterPort -r master -o $slavePort &
+    ./tsd -p $slavePort -r slave -o $masterPort &
 fi
 
 exit 0

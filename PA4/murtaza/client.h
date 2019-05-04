76528cba7bb98a2579d9c945608efc90790f6046
Murtaza Hakimi <murtaza.hakimi@gmail.com>
2019-05-04 14:39:03 -0500

    stress test

--- PA4/murtaza/client.h
+++ PA4/murtaza/client.h
@@ -50,13 +50,12 @@ class IClient
 {
     public:
         void run_client() { run(); }
-
+        virtual IReply processCommand(std::string& cmd) = 0;
     protected:
         /*
          * Pure virtual functions to be implemented by students
          */
         virtual int connectTo() = 0;
-        virtual IReply processCommand(std::string& cmd) = 0;
         virtual int processTimeline() = 0;
 
     private:

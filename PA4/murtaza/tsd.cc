039dc21219866a3b446906e0bc0eaf43bc9fa06a
Murtaza Hakimi <murtaza.hakimi@gmail.com>
2019-05-03 23:12:59 -0500

    update function

--- PA4/murtaza/tsd.cc
+++ PA4/murtaza/tsd.cc
@@ -283,7 +283,7 @@ class HealthServiceImpl final : public HealthService::Service {
         Message m;
         m.set_username(request->client());
         m.set_msg(request->post());
-        google::protobuf::Timestamp* timestamp = request->timestamp();
+        google::protobuf::Timestamp* timestamp(request->timestamp());
         m.set_allocated_timestamp(timestamp);
         std::vector<Client*>::const_iterator it;
         for(it = c->client_followers.begin(); it!=c->client_followers.end(); it++) {
